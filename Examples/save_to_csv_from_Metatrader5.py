6819ae78d486357cfad3694ca17758722d9d1222
Oleg Shpagin <info@wiseplat.org>
2022-04-13 19:47:42 +0300

    + updated example

--- Examples/save_to_csv_from_Metatrader5.py
+++ Examples/save_to_csv_from_Metatrader5.py
@@ -6,7 +6,8 @@ from SharesDataLoader.DataMetatrader import DataMetatrader
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    ticket = "LKOH"
+    ticket = "GAZP"
+    prefix = 'TQBR.'
     timeframe = "D1"
     how_many_bars = 50000
 
@@ -17,11 +18,11 @@ if __name__ == '__main__':
     load_data = DataMetatrader(ticket)
     load_data.ConnectToMetatrader5(path=f"C:\Program Files\FINAM MetaTrader 5\terminal64.exe")
 
-    data = load_data.GetShareDataFromMetatrader(ticket=ticket, timeframe=timeframe, utc_till=utc_till, how_many_bars=how_many_bars, remove_last_bar=True, upper_heading=False)
-    print(data)
+    # data = load_data.GetShareDataFromMetatrader(ticket=ticket, timeframe=timeframe, utc_till=utc_till, how_many_bars=how_many_bars, remove_last_bar=True, upper_heading=False)
+    # print(data)
 
     # create CSV file
-    # load_data.ExportToCsvFromMetatrader(ticket=ticket, timeframe=timeframe, utc_till=utc_till, how_many_bars=how_many_bars, remove_last_bar=True, export_dir="csv", upper_heading=False)
+    load_data.ExportToCsvFromMetatrader(ticket=ticket, timeframe=timeframe, utc_till=utc_till, how_many_bars=how_many_bars, remove_last_bar=True, export_dir="csv", prefix=prefix, upper_heading=False)
 
     # # work with DB
     # load_data.ConnectToDb( host="192.168.0.200",
@@ -32,4 +33,4 @@ if __name__ == '__main__':
     #
     # # data = load_data.GetShareDataFromDb(ticket, timeframe, how_many_bars, upper_heading=False)
     # # print(data)
-    # load_data.ExportToCsvFromDb(ticket, timeframe, how_many_bars, export_dir="csv", upper_heading=True)
+    # load_data.ExportToCsvFromDb(ticket, timeframe, how_many_bars, export_dir="csv", prefix=prefix, upper_heading=True)
