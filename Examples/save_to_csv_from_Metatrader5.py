a1b7b21bcaf49729973905687ffc91f6f338ea34
Oleg Shpagin <info@wiseplat.org>
2022-04-13 19:40:46 +0300

    + updated example

--- Examples/save_to_csv_from_Metatrader5.py
+++ Examples/save_to_csv_from_Metatrader5.py
@@ -6,7 +6,7 @@ from SharesDataLoader.DataMetatrader import DataMetatrader
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    ticket = "GAZP"
+    ticket = "LKOH"
     timeframe = "D1"
     how_many_bars = 50000
 
@@ -17,18 +17,19 @@ if __name__ == '__main__':
     load_data = DataMetatrader(ticket)
     load_data.ConnectToMetatrader5(path=f"C:\Program Files\FINAM MetaTrader 5\terminal64.exe")
 
-    # data = load_data.GetShareDataFromMetatrader(ticket=ticket, timeframe=timeframe, utc_till=utc_till, how_many_bars=how_many_bars, remove_last_bar=True)
-    # print(data)
+    data = load_data.GetShareDataFromMetatrader(ticket=ticket, timeframe=timeframe, utc_till=utc_till, how_many_bars=how_many_bars, remove_last_bar=True, upper_heading=False)
+    print(data)
 
     # create CSV file
-    load_data.ExportToCsvFromMetatrader(ticket=ticket, timeframe=timeframe, utc_till=utc_till, how_many_bars=how_many_bars, remove_last_bar=True, export_dir="csv")
+    # load_data.ExportToCsvFromMetatrader(ticket=ticket, timeframe=timeframe, utc_till=utc_till, how_many_bars=how_many_bars, remove_last_bar=True, export_dir="csv", upper_heading=False)
 
     # # work with DB
     # load_data.ConnectToDb( host="192.168.0.200",
     #                         user="sharesuser",
     #                         passwd="SomePassword123",
     #                         db="shares")
-    # # load_data.always_get_share_data(ticket=ticket, timeframe=timeframe)
-    # data = load_data.GetShareDataFromDb(ticket, timeframe, how_many_bars)
-    # print(data)
-    # load_data.ExportToCsvFromDb(ticket, timeframe, how_many_bars, export_dir="csv")
+    # #load_data.always_get_share_data(ticket=ticket, timeframe=timeframe)
+    #
+    # # data = load_data.GetShareDataFromDb(ticket, timeframe, how_many_bars, upper_heading=False)
+    # # print(data)
+    # load_data.ExportToCsvFromDb(ticket, timeframe, how_many_bars, export_dir="csv", upper_heading=True)
